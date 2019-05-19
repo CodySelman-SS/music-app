d1383b5fbf03489fcc7bddccf0608573fe41c3a7
CodySelman-SS <cody.selman@sharpspring.com>
2019-05-19 12:47:35 -0400

    Basic request logic in place

--- src/App.js
+++ src/App.js
@@ -8,29 +8,43 @@ class App extends React.Component {
     };
   }
 
-  handleClick() {
-    // TODO
+  async handleSubmit() {
+    console.log(`search query: ${this.state.searchText}`);
+    console.log(await this.getAlbums(this.state.searchText));
   }
 
   handleChange(e) {
-    // TODO
+    this.setState({
+      searchText: e.target.value,
+    });
   }
 
-    render() {
-      return(
-        <div>
-          <input
-            onChange = { e => this.handleChange(e) }
-          />
-          <button
-            type = "submit"
-            onClick = {() => this.handleSubmit()}
-          >
-            Submit
-          </button>
-        </div>
-      )
-    }
+  async getAlbums(artistName) {
+    const res = await fetch(this.getUrl(artistName));
+    return await res.json();
+  }
+
+  getUrl(artistName) {
+    const encodedName = encodeURI(artistName);
+    console.log(`encodedName: ${encodedName}`)
+    return `https://itunes.apple.com/search?term=${encodedName}&entity=album`
+  }
+
+  render() {
+    return(
+      <div>
+        <input
+          onChange = { e => this.handleChange(e) }
+        />
+        <button
+          type = "submit"
+          onClick = {() => this.handleSubmit()}
+        >
+          Submit
+        </button>
+      </div>
+    )
+  }
 }
 
 export default App;
