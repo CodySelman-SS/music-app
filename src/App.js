83528679b028a8b1843e29f0d1c17fe7b11142fe
CodySelman-SS <cody.selman@sharpspring.com>
2019-05-19 12:27:35 -0400

    Stubbed out basic form and handlers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
-// import './App.css';
 
-function App() {
-  return (
-    <div>
-    ~*~*~*~*~*~*~*~*~*~*
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+    };
+  }
+
+  handleClick() {
+    // TODO
+  }
+
+  handleChange(e) {
+    // TODO
+  }
+
+    render() {
+      return(
+        <div>
+          <input
+            onChange = { e => this.handleChange(e) }
+          />
+          <button
+            type = "submit"
+            onClick = {() => this.handleSubmit()}
+          >
+            Submit
+          </button>
+        </div>
+      )
+    }
 }
 
 export default App;
