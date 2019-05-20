1e6875dad6c8a3d69843706050f75cba3e7dc7b9
CodySelman-SS <cody.selman@sharpspring.com>
2019-05-19 20:46:11 -0400

    AlbumCard now roughly styled

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,13 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+// import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+// import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+// import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+// import SkipNextIcon from '@material-ui/icons/SkipNext';
+
+const styles = theme => ({
+  card: {
+    display: 'flex',
+    backgroundColor: '#e5dcdb',
+    backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23aca192' fill-opacity='0.4' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E")`,
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 100,
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  },
+});
 
 const AlbumCard = props => {
+  const { classes, theme } = props;
+
   return(
-    <li>
-      <h3>{props.albumName}</h3>
-      <h4>{props.releaseYear}</h4>
-      <img src={props.imgSrc} alt={props.albumName} />
-    </li>
+    <Card className={classes.card}>
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5">
+            { props.albumName }
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            { props.releaseYear }
+          </Typography>
+        </CardContent>
+        {/* <div className={classes.controls}>
+          <IconButton aria-label="Previous">
+            {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
+          </IconButton>
+          <IconButton aria-label="Play/pause">
+            <PlayArrowIcon className={classes.playIcon} />
+          </IconButton>
+          <IconButton aria-label="Next">
+            {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
+          </IconButton>
+        </div> */}
+      </div>
+      <CardMedia
+        className={classes.cover}
+        image={props.imgSrc}
+        title={props.albumName}
+      />
+    </Card>
   );
 }
 
@@ -15,6 +77,8 @@ AlbumCard.propTypes = {
   albumName: PropTypes.string.isRequired,
   releaseYear: PropTypes.number.isRequired,
   imgSrc: PropTypes.string.isRequired,
+  classes: PropTypes.object.isRequired,
+  // theme: PropTypes.object.isRequired,
 }
 
-export default AlbumCard;
+export default withStyles(styles, { withTheme: true })(AlbumCard);
