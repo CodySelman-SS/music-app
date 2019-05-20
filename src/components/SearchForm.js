94bd9483901b0fd1c1e57976b363837646912724
CodySelman-SS <cody.selman@sharpspring.com>
2019-05-19 20:45:56 -0400

    SearchForm now has background, updated PropTypes

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -18,6 +18,8 @@ const styles = {
     alignItems: 'center',
     width: 400,
     maxWidth: '90%',
+    backgroundColor: '#e5dcdb',
+    backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23aca192' fill-opacity='0.4' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E")`,
   },
   input: {
     marginLeft: 8,
@@ -58,6 +60,7 @@ const SearchForm = props => {
 SearchForm.propTypes = {
   onChange: PropTypes.func.isRequired,
   onClick: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
 }
 
 export default withStyles(styles)(SearchForm);
