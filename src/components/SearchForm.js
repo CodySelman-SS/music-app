c6fe4b31d4330c26926a4909120e97fc60252655
CodySelman-SS <cody.selman@sharpspring.com>
2019-05-19 20:09:29 -0400

    SearchForm component now styled

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,19 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+
+const styles = {
+  grid: {
+    justify: 'center',
+  },
+  root: {
+    margin: '12px 0',
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+    maxWidth: '90%',
+  },
+  input: {
+    marginLeft: 8,
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+};
 
 const SearchForm = props => {
+  const { classes } = props;
+
   return(
-    <form>
-      <input
+    <Grid
+      container
+      justify='center'
+      alignItems='center'
+    >
+      <Paper className={classes.root} elevation={1}>
+        <InputBase
+          className={classes.input}
+          placeholder="Search Artist Name"
           onChange = { e => props.onChange(e) }
         />
-        <button
-          type = "submit"
+        <IconButton
+          className={classes.iconButton}
+          aria-label="Search"
           onClick = { e => props.onClick(e) }
         >
-          Submit
-        </button>
-    </form>
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    </Grid>
   );
 }
 
@@ -22,4 +60,4 @@ SearchForm.propTypes = {
   onClick: PropTypes.func.isRequired,
 }
 
-export default SearchForm;
+export default withStyles(styles)(SearchForm);
